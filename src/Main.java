84c1641364309b26fd4a01a94edf27af2ed34b90
Julien Villanti <villantijulien@gmail.com>
2023-12-13 19:53:18 -0500

    created WheeledTransportation and Train class. Tested briefly.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
+import WheeledTransportation.WheeledTransportation;
+import WheeledTransportation.Train;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        WheeledTransportation w1 = new WheeledTransportation(4, 250);
+        Train t1 = new Train(10, 600, 25, "Paris","Berlin");
+        Train t2 = new Train(20, 450, 34, "Montréal","New York");
+        System.out.println(w1);
+        System.out.println(t1);
+        System.out.println(t2);
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
--- src/WheeledTransportation/WheeledTransportation.java
+++ src/WheeledTransportation/WheeledTransportation.java
@@ -1,4 +1,70 @@
 package WheeledTransportation;
 
 public class WheeledTransportation {
+    private int numOfWheels;
+    private double maxSpeed;
+    private long serialNumber;
+    private static long nextSerialNumber = 1;
+
+
+    public WheeledTransportation() {
+        //default constructor
+        serialNumber = generateSerialNumber();
+    }
+
+    public WheeledTransportation(WheeledTransportation w1) {
+        //copy constructor
+        numOfWheels = w1.numOfWheels;
+        maxSpeed = w1.maxSpeed;
+        serialNumber = w1.serialNumber;
+        serialNumber = generateSerialNumber();
+    }
+
+    public WheeledTransportation(int nw, double ms) {
+        //parameter constructor
+        numOfWheels = nw;
+        maxSpeed = ms;
+        serialNumber = generateSerialNumber();
+    }
+
+    public int getNumOfWheels() {
+        return numOfWheels;
+    }
+
+    public void setNumOfWheels(int numOfWheels) {
+        this.numOfWheels = numOfWheels;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public long getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(long serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "This is a wheeled transportation. " +
+                "The number of wheels is " + numOfWheels +
+                ". Its maximum speed is" + maxSpeed +
+                " km/h. The serial number is " + serialNumber + ".";
+    }
+
+    private static long generateSerialNumber() {
+        return nextSerialNumber++;
+    }
 }
